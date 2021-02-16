1dcced15fadc70be42ec5515c9f5ed7422b75653
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2021-02-16 18:39:49 +0100

    Added another exception handler to catch any exception

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
@@ -21,10 +21,23 @@ public class CustomerRestExceptionHandler {
 		
 		//return ResponseEntity
 		
-		
 		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 	}
 	
+	//Add another exception handler ... to catch any exception (catch all)
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(Exception exc) {
+		
+		//create CustomerErrorResponse
+		
+		CustomerErrorResponse error = 
+				new CustomerErrorResponse(HttpStatus.BAD_REQUEST.value(),
+						exc.getMessage(), System.currentTimeMillis());
+		
+		//return ResponseEntity
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
 	
 
 }
